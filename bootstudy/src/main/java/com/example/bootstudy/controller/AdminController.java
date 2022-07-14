9320c7b8d5c0c57901f9ed39c73f805d35b09b5d
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2022-07-14 22:37:07 +1000

    changed listBS

--- bootstudy/src/main/java/com/example/bootstudy/controller/AdminController.java
+++ bootstudy/src/main/java/com/example/bootstudy/controller/AdminController.java
@@ -13,6 +13,7 @@ import com.example.bootstudy.service.RoleService;
 import com.example.bootstudy.service.UserService;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.Set;
 
 @Controller
@@ -46,13 +47,15 @@ public class AdminController {
 
         User user = (User) userDetailsService.loadUserByUsername(logedInUser.getUsername());
         System.out.println("ID: " +user.getId() +" " +user.toString() +"roles: "+ user.getRoles());
-        model.addAttribute("user",new User());
+        model.addAttribute("loggedInUser",user);
+
+        model.addAttribute("new_user",new User());
 
 
         model.addAttribute("usersSet",userService.getAll());
         model.addAttribute("RolesSet",roleService.getAllRoles());
         return "admins_pages/listBS";
-
+//        return "admins_pages/заметки 2_1";
 
     }
 
@@ -81,89 +84,31 @@ public class AdminController {
     }
 
 
-//    @PostMapping("/delete/{id}")
-//    public String delete(@PathVariable("id") Long id) {
-//        System.out.println("user with id " +id +" " + userService.getUserById(id) +"was removed");
-//        userService.deleteUserById(id);
-//        return "redirect:/admin";
-//    }
-
-    @PostMapping("/delete/") //типо тут можно попробовать с requestparam вместо pathvariable
-    public String delete(@ModelAttribute("user") User user,@RequestParam(value="idDelete") Long id){
-       // model.addAttribute("idDelete",id);
-        System.out.println("User with id " +  user.getId());
-        user=userService.getUserById(id);
-        userService.deleteUser(user);
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        System.out.println("user with id " +id +" " + userService.getUserById(id) +"was removed");
+        userService.deleteUserById(id);
         return "redirect:/admin";
     }
 
+//    @PostMapping("/delete/") //типо тут можно попробовать с requestparam вместо pathvariable
+//    public String delete(@ModelAttribute("user") User user,@RequestParam(value="idDelete") Long id){
+//       // model.addAttribute("idDelete",id);
+//        System.out.println("User with id " +  user.getId());
+//        user=userService.getUserById(id);
+//        userService.deleteUser(user);
+//        return "redirect:/admin";
+//    }
+
     //Code for user's page
     @GetMapping("/user")
-    public String showUserPage(Model model, @AuthenticationPrincipal UserDetails logedInUser) {
+    public String showUserPage(Model model,
+                               @AuthenticationPrincipal UserDetails logedInUser) {
+
         User user = (User) userDetailsService.loadUserByUsername(logedInUser.getUsername());
-        model.addAttribute("user",user);
+        model.addAttribute("loggedInUser",user);
         return  "userPage";
 
     }
-
-
-    //Adding roles
-
-
-
-
-
-//    @GetMapping("/showFormRoles")
-//    public String showFormForAddingRoles(Model model, Role role) {
-//        model.addAttribute("Role", role);
-//        return "admins_pages/newRole";
-//    }
-//
-//
-//    @PostMapping("saveRole")
-//    public String saveRole( @Valid @ModelAttribute("Role") Role role
-//                            ,BindingResult bindingResult)  {
-//
-//        if (bindingResult.hasErrors()){
-//            return  "admins_pages/newRole";
-//        }
-//
-//
-//        roleService.saveRole(role);
-//        return "redirect:/admin ";
-//
-//    }
-//
-//    @GetMapping("editRole/{id}")
-//    public String showEditRoleForm(@PathVariable("id") Long id, Model model) {
-//        model.addAttribute("Role",roleService.getRoleById(id));
-//        return "admins_pages/editRoleForm";
-//    }
-//
-//    @PostMapping("/role{id}")
-//    public String updateRole(@Valid @ModelAttribute("Role") Role role,BindingResult bindingResult ) {
-//
-//        if (bindingResult.hasErrors()) {
-//            return  "admins_pages/editRoleForm";
-//        }
-//
-//
-//        roleService.update(role);
-//        return "redirect:/admin";
-//    }
-//
-//    @GetMapping("/role{id}")
-//    public String deleteRole(@PathVariable("id") Long id) {
-//        roleService.deleteRoleById(id);
-//        return "redirect:/admin";
-//    }
-
-
-
-
-
-
-
-
 
 }
